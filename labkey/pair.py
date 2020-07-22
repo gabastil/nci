5ed30f1681395e6e124935e6351b41596b8ecafd
Glenn Abastillas <glenn@Glenn.local>
2020-07-22 11:24:38 -0400

    Added pair.py and __init__.py—successful run

--- labkey/pair.py
+++ labkey/pair.py
@@ -4,22 +4,38 @@ from pprint import PrettyPrinter
 
 def load(file):
     with open(file) as fin:
-        return [__.strip() for __ in fin.readlines() if __.strip()]
+        return [__.strip() for __ in fin.readlines() if __.strip()][1:]
 
+def clean(func):
+    def function(A, B):
+        A, B = set(A), set(B)
+        return func(A, B)
+    return function
+
+@clean
 def detect(task_ids, backlog):
-    A, B = set(task_ids), set(backlog)
-    return A.intersection(B)
+    return task_ids.intersection(backlog)
+
+@clean
+def no_matches(task_ids, backlog):
+    return task_ids.difference(backlog)
 
 if __name__=='__main__':
-    # Check 'Completed' Task IDs
-    enhancements = 'resources/enhancements.txt'
-    backlog_complete = 'resources/backlog_complete.txt'
-    backlog_incomplete = 'resources/backlog_incomplete.txt'
+    # import os
+
+    # print(os.getcwd())
+    enhancements = load('labkey/resources/enhancements.txt')
+    backlog_complete = load('labkey/resources/backlog_complete.txt')
+    backlog_incomplete = load('labkey/resources/backlog_incomplete.txt')
 
     completed = detect(enhancements, backlog_complete)
     incompleted = detect(enhancements, backlog_incomplete)
+    unmatched = (no_matches(enhancements, backlog_incomplete) &
+                 no_matches(enhancements, backlog_complete))
 
 
     pp = PrettyPrinter(indent=4)
-    pp.pprint(f'Completed {completed}')
-    pp.pprint(f'Incomplete {incompleted}')
+    pp.pprint([int(__) for __ in completed])
+    pp.pprint([int(__) for __ in incompleted])
+    pp.pprint(list(unmatched))
+    print(len(completed | incompleted | unmatched))
